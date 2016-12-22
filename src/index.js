217aa62ca23878e34cd30c69fcefc4b664de8eab
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2016-12-22 11:24:13 +0530

    clean up code

--- src/index.js
+++ src/index.js
@@ -48,13 +48,13 @@ const samplingProgramConfig = {
 
 const config = {
   canvas: null,
+  imgUrls: [],
   mode: modes.PARABOLIC,
-  direction: directions.HORIZONTAL,
+  direction: directions.VERTICAL,
   factor: 0.1,
   multiplier: 0.1,
   originOffset: -0.5,
   recover: 1,
-  imgUrls: [],
   fps: 60,
 };
 
@@ -72,6 +72,116 @@ const tileSize = {
   W: 0,
   H: 0,
 };
+
+
+function fetchShaderSrc(shaderUrl) {
+  return fetch(shaderUrl).then(resp => resp.text());
+}
+
+
+async function initShader(shaderType, shaderUrl) {
+  const shaderSrc = await fetchShaderSrc(shaderUrl);
+
+  const shader = gl.createShader(shaderType);
+  gl.shaderSource(shader, shaderSrc);
+
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw Error(gl.getShaderInfoLog(shader));
+  }
+
+  return shader;
+}
+
+
+async function initProgram(program, programConfig) {
+  const vertexShader = await initShader(gl.VERTEX_SHADER, programConfig.vertexShader);
+  gl.attachShader(program, vertexShader);
+
+  const fragmentShader = await initShader(gl.FRAGMENT_SHADER, programConfig.fragmentShader);
+  gl.attachShader(program, fragmentShader);
+
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    throw Error(gl.getProgramInfoLog(program));
+  }
+  gl.useProgram(program);
+
+  programConfig.attributes.forEach((attribute) => {
+    program[attribute] = gl.getAttribLocation(program, attribute);
+    gl.enableVertexAttribArray(program[attribute]);
+  });
+
+  programConfig.uniforms.forEach((uniform) => {
+    program[uniform] = gl.getUniformLocation(program, uniform);
+  });
+}
+
+
+function setUniforms() {
+  // TODO: choose better name
+  switch (config.direction) {
+    case directions.VERTICAL:
+      config.factor = [config.factor, 0];
+      config.multiplier = [config.multiplier, 0];
+      config.originOffset = [0, config.originOffset];
+      config.recover = [1, config.recover];
+      break;
+
+    case directions.HORIZONTAL:
+      config.factor = [0, config.factor];
+      config.multiplier = [0, config.multiplier];
+      config.originOffset = [config.originOffset, 0];
+      config.recover = [config.recover, 1];
+      break;
+
+    default:
+  }
+
+  gl.useProgram(mainProgram);
+  gl.uniform1i(mainProgram.uMode, config.mode);
+  gl.uniform2fv(mainProgram.uFactor, config.factor);
+  gl.uniform2fv(mainProgram.uMultiplier, config.multiplier);
+  gl.uniform2fv(mainProgram.uOriginOffset, config.originOffset);
+  gl.uniform2fv(mainProgram.uRecover, config.recover);
+}
+
+
+function fetchImages() {
+  return Promise.all(config.imgUrls.map((imgUrl) => {
+    const img = new Image();
+    img.src = imgUrl;
+
+    return new Promise((resolve) => {
+      img.onload = () => {
+        if (tileSize.W === 0 || tileSize.H === 0) {
+          tileSize.W = img.width;
+          tileSize.H = img.height;
+        }
+        resolve(img);
+      };
+    });
+  }));
+}
+
+
+async function initTextures() {
+  const imgs = await fetchImages();
+  imgs.forEach((img, idx) => {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+
+    samplingScreen.tiles[idx].texture = texture;
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+  });
+}
 
 
 function initSamplingFrameBuffer() {
@@ -96,6 +206,35 @@ function initSamplingFrameBuffer() {
 
   gl.bindFramebuffer(gl.FRAMEBUFFER, null);
   gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+
+function initDataBuffers() {
+  mainScreen.initDataBuffers();
+  samplingScreen.tiles.forEach((tile, idx) => {
+    const tileOffset = {
+      x: 0,
+      y: 0,
+    };
+
+    switch (config.direction) {
+      case directions.VERTICAL:
+        tileOffset.y = -1 * (2 * idx);
+        break;
+
+      case directions.HORIZONTAL:
+        tileOffset.x = 2 * idx;
+        break;
+
+      default:
+    }
+
+    tile.initDataBuffers(tileOffset);
+  });
+
+  initSamplingFrameBuffer();
+
+  return Promise.resolve();
 }
 
 
@@ -150,143 +289,6 @@ function render(HRTimestamp) {
 }
 
 
-function fetchShaderSrc(shaderUrl) {
-  return fetch(shaderUrl).then(resp => resp.text());
-}
-
-
-async function initShader(shaderType, shaderUrl) {
-  const shaderSrc = await fetchShaderSrc(shaderUrl);
-
-  const shader = gl.createShader(shaderType);
-  gl.shaderSource(shader, shaderSrc);
-
-  gl.compileShader(shader);
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    throw Error(gl.getShaderInfoLog(shader));
-  }
-
-  return shader;
-}
-
-
-function setUniforms() {
-  switch (config.direction) {
-    case directions.VERTICAL:
-      config.factor = [config.factor, 0];
-      config.multiplier = [config.multiplier, 0];
-      config.originOffset = [0, config.originOffset];
-      config.recover = [1, config.recover];
-      break;
-
-    case directions.HORIZONTAL:
-      config.factor = [0, config.factor];
-      config.multiplier = [0, config.multiplier];
-      config.originOffset = [config.originOffset, 0];
-      config.recover = [config.recover, 1];
-      break;
-
-    default:
-  }
-
-  gl.uniform1i(mainProgram.uMode, config.mode);
-  gl.uniform2fv(mainProgram.uFactor, config.factor);
-  gl.uniform2fv(mainProgram.uMultiplier, config.multiplier);
-  gl.uniform2fv(mainProgram.uOriginOffset, config.originOffset);
-  gl.uniform2fv(mainProgram.uRecover, config.recover);
-}
-
-
-async function initProgram(program, programConfig) {
-  const vertexShader = await initShader(gl.VERTEX_SHADER, programConfig.vertexShader);
-  gl.attachShader(program, vertexShader);
-
-  const fragmentShader = await initShader(gl.FRAGMENT_SHADER, programConfig.fragmentShader);
-  gl.attachShader(program, fragmentShader);
-
-  gl.linkProgram(program);
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    throw Error(gl.getProgramInfoLog(program));
-  }
-  gl.useProgram(program);
-
-  programConfig.attributes.forEach((attribute) => {
-    program[attribute] = gl.getAttribLocation(program, attribute);
-    gl.enableVertexAttribArray(program[attribute]);
-  });
-
-  programConfig.uniforms.forEach((uniform) => {
-    program[uniform] = gl.getUniformLocation(program, uniform);
-  });
-}
-
-
-function initDataBuffers() {
-  mainScreen.initDataBuffers();
-  samplingScreen.tiles.forEach((tile, idx) => {
-    const tileOffset = {
-      x: 0,
-      y: 0,
-    };
-
-    switch (config.direction) {
-      case directions.VERTICAL:
-        tileOffset.y = -1 * (2 * idx);
-        break;
-
-      case directions.HORIZONTAL:
-        tileOffset.x = 2 * idx;
-        break;
-
-      default:
-    }
-
-    tile.initDataBuffers(tileOffset);
-  });
-
-  initSamplingFrameBuffer();
-
-  return Promise.resolve();
-}
-
-
-function fetchImages() {
-  return Promise.all(config.imgUrls.map((imgUrl) => {
-    const img = new Image();
-    img.src = imgUrl;
-
-    return new Promise((resolve) => {
-      img.onload = () => {
-        if (tileSize.W === 0 || tileSize.H === 0) {
-          tileSize.W = img.width;
-          tileSize.H = img.height;
-        }
-        resolve(img);
-      };
-    });
-  }));
-}
-
-
-async function initTextures() {
-  const imgs = await fetchImages();
-  imgs.forEach((img, idx) => {
-    const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-
-    samplingScreen.tiles[idx].texture = texture;
-
-    gl.bindTexture(gl.TEXTURE_2D, null);
-  });
-}
-
-
 async function init(_config) {
   Object.assign(config, _config);
 
@@ -294,9 +296,9 @@ async function init(_config) {
   gl.clearColor(0, 0, 0, 1);
 
   mainProgram = gl.createProgram();
-  mainScreen = new Screen(gl, mainProgram);
-
   samplingProgram = gl.createProgram();
+
+  mainScreen = new Screen(gl, mainProgram);
   samplingScreen = new Screen(gl, samplingProgram, config.imgUrls.length);
 
   await Promise.all([
